6d93144521c19e8f7f0f559f17c2852d053eca82
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-03 16:34:21 +0900

    feat: disable squash button

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,18 +1,19 @@
-function checkBranch(){
-    chrome.storage.sync.get(({branchesList:[]}), (data)=>{  
+function checkBranch() {
+    chrome.storage.sync.get(({ branchesList: [] }), (data) => {
         const targetBranch = document.querySelector("span.commit-ref span.css-truncate-target").innerText
-        if (data.branchesList.includes(targetBranch)){
-            const button = document.querySelector("button.js-merge-box-button-squash")
-            if (button){
-                button.disabled=true
-            }else{
-                setTimeout(checkBranch,125)
+        if (data.branchesList.includes(targetBranch)) {
+            const items = document.getElementsByClassName("btn-group-squash")
+            if (items.length > 0) {
+                for (let i = 0; i < items.length; i++) {
+                    items[i].disabled = true
+                }
+            } else {
+                setTimeout(checkBranch, 125)
             }
-        }    
+        }
     })
 }
 
-
-window.onload = function() {
+window.onload = function () {
     setTimeout(checkBranch, 125)
 }
