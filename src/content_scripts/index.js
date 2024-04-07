51d024fa0569339b279a0cfd09b4a615ae672e9c
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-07 19:37:46 +0900

    feat: update script

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,21 +1,42 @@
+const TARGET_BRANCH_ELEMENT = "span.commit-ref span.css-truncate-target"
+
 function checkBranch() {
-  chrome.storage.sync.get({ branchesList: [] }, (data) => {
-    const targetBranch = document.querySelector(
-      "span.commit-ref span.css-truncate-target",
-    ).innerText;
-    if (data.branchesList.includes(targetBranch)) {
-      const button = document.querySelector(
-        "button.js-merge-box-button-squash",
-      );
-      if (button) {
-        button.disabled = true;
-      } else {
-        checkBranch();
-      }
-    }
-  });
+    chrome.storage.sync.get({ branchesList: [] }, (data) => {
+        const targetBranch = getInnerText(TARGET_BRANCH_ELEMENT);
+        if (!targetBranch) return;
+        if (data.branchesList.includes(targetBranch)) {
+            disabledElement("button.btn-group-squash")
+            disabledElement("button.js-merge-box-button-squash")
+        }
+    });
 }
 
-window.onload = () => {
-  checkBranch();
-};
+function getInnerText(element) {
+    return getElement(element).innerText;
+}
+
+function getElement(element) {
+    return document.querySelector(
+        element,
+    );
+}
+
+function disabledElement(element) {
+    waitElement(element, () => {
+        getElement(element).disabled = true;
+    })
+}
+
+function waitElement(element, callback) {
+    const timerId = setInterval(elementLoaded, 125);
+    function elementLoaded() {
+        const targetElement = getElement(element);
+        if (targetElement) {
+            clearInterval(timerId);
+            callback();
+        }
+    }
+}
+
+console.log("content script loaded");
+waitElement(TARGET_BRANCH_ELEMENT, checkBranch)
