893c8ad56587a592e68202af751ef578bf27068b
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-07 09:21:21 +0900

    feat: add biome (#3)

--- src/options/options.js
+++ src/options/options.js
@@ -1,23 +1,22 @@
 const saveButton = document.getElementById("update");
 const branchesList = document.getElementById("branches");
 const errors = document.getElementById("errors");
-chrome.storage.sync.get(({branchesList:[]}), (data)=>{
-    branchesList.value=data.branchesList.join('\n')
-})
-saveButton.addEventListener("click", function(){
-    const branches = branchesList.value;
-    errors.innerText=""
-    const blockList =[];
-    for (const b of branches.split('\n')){
-        const branch = b.trim();
-        if (branch.indexOf(" ") > -1){
-            errors.innerText=`Branch '${branch}' contains whitespace`
-            return;
-        }
-        blockList.push(branch);
+chrome.storage.sync.get({ branchesList: [] }, (data) => {
+  branchesList.value = data.branchesList.join("\n");
+});
+saveButton.addEventListener("click", () => {
+  const branches = branchesList.value;
+  errors.innerText = "";
+  const blockList = [];
+  for (const b of branches.split("\n")) {
+    const branch = b.trim();
+    if (branch.indexOf(" ") > -1) {
+      errors.innerText = `Branch '${branch}' contains whitespace`;
+      return;
     }
-    chrome.storage.sync.set(({branchesList:blockList}), ()=>{
-        errors.innerText=`Saved`
-    })
-    
-})
+    blockList.push(branch);
+  }
+  chrome.storage.sync.set({ branchesList: blockList }, () => {
+    errors.innerText = "Saved";
+  });
+});
